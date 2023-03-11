52384c3fd61d7f50ae9bca83992383015b582601
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-03-11 18:58:40 +0300

    initial commit

---
+++ _news/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from _news.models import Article
+from _news.models import Journalist
+
+admin.site.register(Article)
+admin.site.register(Journalist)
---
+++ _news/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Journalist(models.Model):
+    name = models.CharField(max_length=120)
+    surname = models.CharField(max_length=120)
+    biography = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+    
+
+
+class Article(models.Model):
+    journalist = models.ForeignKey(Journalist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=200)
+    text = models.TextField()
+    city = models.CharField(max_length=120)
+    publish_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
